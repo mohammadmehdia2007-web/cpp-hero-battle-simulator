0801a6e2a0b504f6e2d6d78861c60cf34396a530
mehdi <mohammad.mehdi.a.2007@gmail.com>
2026-06-17 11:54:36 +0330

    feat(main) : add print_ hero_menu

--- main.cpp
+++ main.cpp
@@ -10,3 +10,18 @@
 
 using namespace std;
 
+void print_hero_menu()
+{
+    cout << "1. doctor white  (healer)\n"
+         << "2. amin imeni  (attacker)\n"
+         << "3. dny goleng  (defender)\n"
+         << "4. taha koochike (healer)\n"
+         << "5. taha bozorge(attacker)\n";
+}
+
+int main()
+{
+
+
+    return 0;
+}
