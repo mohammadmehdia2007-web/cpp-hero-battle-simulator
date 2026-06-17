5167bc35b6abd57247d5142edb817ef7ae6636d1
mehdi <mohammad.mehdi.a.2007@gmail.com>
2026-06-17 11:49:29 +0330

    feat(main) : start main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include "whitedoctor.h"
+#include "aminimni.h"
+#include "dannygolang.h"
+#include "tahas.h"
+#include "tahab.h"
+
+using namespace std;
+
